095d9f5dbcefb705519b104fb2c39486fbd7b18d
Chris Aumann <me@chr4.org>
2018-05-13 12:40:41 +0200

    Add test database support to utils/database/sqlite

--- utils/database/sqlite/sqlite.go
+++ utils/database/sqlite/sqlite.go
@@ -6,21 +6,63 @@ import (
 	_ "github.com/golang-migrate/migrate/source/file"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
+	"os"
 )
 
-// Open database and run migrations
+// Open production database and run migrations
 func OpenAndMigrate() (db *sqlx.DB, err error) {
 	db, err = sqlx.Open("sqlite3", "database.sqlite3")
 	if err != nil {
 		return
 	}
 
+	err = runMigrations(db, ".")
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// Delete and re-create test database, run migrations and insert test data
+func InitTestDB(relPath string) (db *sqlx.DB, err error) {
+	testDBPath := relPath + "/test.sqlite3"
+
+	// Remove test database if it exists
+	_, err = os.Stat(testDBPath)
+	if err == nil {
+		err2 := os.Remove(testDBPath)
+		if err2 != nil {
+			err = err2
+			return
+		}
+	}
+
+	db, err = sqlx.Open("sqlite3", testDBPath)
+	if err != nil {
+		return
+	}
+
+	err = runMigrations(db, relPath)
+	if err != nil {
+		return
+	}
+
+	// err = addTestData(db, relPath)
+	// if err != nil {
+	// 	return
+	// }
+
+	return
+}
+
+func runMigrations(db *sqlx.DB, relPath string) (err error) {
 	driver, err := sqlite3.WithInstance(db.DB, &sqlite3.Config{})
 	if err != nil {
 		return
 	}
 
-	m, err := migrate.NewWithDatabaseInstance("file://db/migrations", "sqlite3", driver)
+	m, err := migrate.NewWithDatabaseInstance("file://"+relPath+"/db/migrations", "sqlite3", driver)
 	if err != nil {
 		return
 	}
